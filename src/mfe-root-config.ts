aa6948f8266d47fff3ffd719d0221cd2144b5288
Mike Will <Mike.Rego@baymetrics.com.br>
2025-06-17 18:42:55 -0300

    implementation of new micro front-ends

--- src/mfe-root-config.ts
+++ src/mfe-root-config.ts
@@ -1,9 +1,15 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
 registerApplication({
-  name: "@mfe/sys-flow",
-  app: () => System.import<LifeCycles>("@mfe/sys-flow"),
-  activeWhen: ["/"],
+  name: "@mfe/auth",
+  app: () => System.import<LifeCycles>("@mfe/auth"),
+  activeWhen: ["/auth"],
+});
+
+registerApplication({
+  name: "@mfe/buildings",
+  app: () => System.import<LifeCycles>("@mfe/buildings"),
+  activeWhen: ["/buildings"],
 });
 
 start({
